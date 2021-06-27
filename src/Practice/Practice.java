d4d3426baff1ad810437353508d20ec76dd39a6a
Harry2915 <harishhibare2915@gmal.com>
2021-06-27 20:43:17 +0530

    anagram

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,24 +1,26 @@
 package Practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Practice {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		System.out.println("Enter the string");
-		String s1= s.nextLine();
-		System.out.println(ispalin(s1));
-	}
-	public static String ispalin(String s) {
-		String s1 ="";
-		for(int i=s.length()-1;i>=0;i--) {
-			s1+=s.charAt(i);
-		}
-		if(s.equalsIgnoreCase(s1))
-			return "is palin";
-		else
+		System.out.println("Enter strings");
+	    String sc = s.nextLine();
+		String sc1 = s.nextLine();
+		System.out.println(isana(sc,sc1));
 		
-		
-		return "not palin";
 	}
+    
+	 public static String isana(String s,String s1) {
+		 char arr[]=s.toLowerCase().toCharArray();
+		 char arr2[]=s1.toLowerCase().toCharArray();
+		 Arrays.sort(arr);
+		 Arrays.sort(arr2);
+		 if(Arrays.equals(arr, arr2))
+			 return "is ana";
+		 
+		 return "not ana";
+	 }
 }
