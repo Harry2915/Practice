404e999075efc7908556f2233ab92afa28b99edb
Harry2915 <harishhibare2915@gmal.com>
2021-06-27 20:34:45 +0530

    Palindrom

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,7 +1,24 @@
 package Practice;
 
+import java.util.Scanner;
+
 public class Practice {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the string");
+		String s1= s.nextLine();
+		System.out.println(ispalin(s1));
+	}
+	public static String ispalin(String s) {
+		String s1 ="";
+		for(int i=s.length()-1;i>=0;i--) {
+			s1+=s.charAt(i);
+		}
+		if(s.equalsIgnoreCase(s1))
+			return "is palin";
+		else
+		
+		
+		return "not palin";
 	}
 }
