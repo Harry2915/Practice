5d7ab304f949d2fc16519689e15f23aaf4f43d41
Harry2915 <harishhibare2915@gmal.com>
2021-06-28 19:51:08 +0530

    right angled triangle pattern

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -5,22 +5,12 @@ import java.util.Scanner;
 
 public class Practice {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		System.out.println("Enter strings");
-	    String sc = s.nextLine();
-		String sc1 = s.nextLine();
-		System.out.println(isana(sc,sc1));
-		
-	}
-    
-	 public static String isana(String s,String s1) {
-		 char arr[]=s.toLowerCase().toCharArray();
-		 char arr2[]=s1.toLowerCase().toCharArray();
-		 Arrays.sort(arr);
-		 Arrays.sort(arr2);
-		 if(Arrays.equals(arr, arr2))
-			 return "is ana";
-		 
-		 return "not ana";
+	   for(int i=0;i<5;i++) {
+		   for(int j=0;j<=i;j++)
+			   System.out.print(" * ");
+		   
+		   
+		   System.out.println();
+	   }
 	 }
 }
