3fad6ea81f0b3dd05490a8cce4bce13f88c77c4b
Harry2915 <harishhibare2915@gmal.com>
2021-06-29 12:33:00 +0530

    HashMap examples

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,30 +1,45 @@
 package Practice;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class Practice {
 	public static void main(String[] args) {
 	 
-	   Scanner sc = new Scanner(System.in);
-	   System.out.println("Enter the string");
-	   String s = sc.nextLine();
-	   System.out.println("Enter finding word");
-	   String find = sc.nextLine();
-	 
-	   System.out.println(count1(s,find));
-	 }
-	public static int count1(String s, String find) {
-		String arr[] = s.split(" ");
-		int count = 0;
-		for(int i=0;i<arr.length;i++) {
-			if(arr[i].equalsIgnoreCase(find))
-				count++;
+		String s1= "my name is Harish Hibare";
+		HashMap <Character,Integer> map = new HashMap();
+		String s=s1.toLowerCase();
+		for(int i=0;i<s.length()-1;i++) {
+			int count = 0;
+			char temp=s.charAt(i);
+			
+			if(s.charAt(i)!='0' && s.charAt(i)!=' ') {
+			for(int j=0;j<s.length()-1;j++) {
+				if(s.charAt(j)==temp) {
+					count++;
+					s.replace(s.charAt(j), '0');
+				}
+			}
+			}
+			map.put(temp, count);
+		}
+		System.out.println(map);
+		int max=0;
+		for (Entry<Character, Integer> entry : map.entrySet()) {
+			if(entry.getValue()>max)
+				max=entry.getValue();
+		}
+		
+		for(Map.Entry<Character,Integer> entry : map.entrySet()) {
+			if(entry.getValue()==max)
+				System.out.println(entry.getKey()+" : "+entry.getValue());
 		}
 		
 		
-		
-		return count;
+	
 	}
 	
 	
