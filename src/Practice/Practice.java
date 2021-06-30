b7ca097287a0dfe32574de32035eafd6a9f32951
Harry2915 <harishhibare2915@gmal.com>
2021-06-30 10:06:30 +0530

    Threads using runnable interface

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -6,28 +6,33 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Scanner;
 
+class A implements Runnable{
+	
+	public void run() {
+		for(int i=0;i<10;i++)
+			System.out.println("IN CLASS A: " +i);
+		
+	}
+	
+}
+
+class B implements Runnable{
+	
+	public void run() {
+		for(int i=0;i<10;i++)
+			System.out.println("IN CLASS B: " +i);
+		
+	}
+	
+}
+
 public class Practice {
 	public static void main(String[] args) {
-	 
-	String s = "0";
-	int count=0;
-	int tempcount=0;
-	for(int i=0;i<s.length()-1;i++) {
-		if(s.charAt(i)=='1')
-		{
-			tempcount++;
-			
-		}
-		else {
-			if(tempcount>count) {
-				count=tempcount;
-				tempcount=0;
-			}else 
-				tempcount=0;
-			
-		}
-	}
-		System.out.println(count);
+	 Thread t1 = new Thread(new A());
+	 Thread t2 = new Thread(new B());
+ 
+	 t1.start();
+	 t2.start();
 	
 	}
 	
