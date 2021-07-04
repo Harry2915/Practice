4a99af9d09ee3e3ad7c266919d93b9cf44e670fc
Harry2915 <harishhibare2915@gmal.com>
2021-07-04 19:53:23 +0530

    hashmap example

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -9,33 +9,50 @@ import java.util.Scanner;
 
 public class Practice {
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int count=0;
-	for(int i=1;i<=100;i++) {
-		int a=i;
 		
-		if(isPrime(a).equals(" is prime"))
-			count++;
-		System.out.println(a+ " " +isPrime(a));
-	}
-	System.out.println();
-	System.out.println(count);
-	
-	}
-	
-	public static String isPrime(int a) {
-	
-		if(a==0||a==1)
-			return "not prime";
-		for(int i=2;i<a;i++) {
-			if(a%i==0) 
-				return "not prime";
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(countword(s));
+		HashMap<Character, Integer> map= new HashMap<Character, Integer>();
+		map.putAll(countword(s));
+		int max=0;
+		for(Map.Entry<Character, Integer> m : map.entrySet()) {
 			
+			if(m.getValue()>max)
+				max=m.getValue();
+		}
+		for(Map.Entry<Character, Integer> m : map.entrySet()) {
+			if(m.getValue()==max)
+				System.out.println(m.getKey()+" "+m.getValue());
+		}
+	}
+public static HashMap<Character, Integer> countword(String s){
+	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+	for(int i=0;i<s.length();i++) {
+		
+		if(s.charAt(i)!='0' && s.charAt(i)!=' ') {
+			int count=0;
+		char temp=s.charAt(i);
+		for(int j=0;j<s.length();j++) {
+			if(s.charAt(j)==temp && s.charAt(j)!='0') {
+				count++;
+				s.replace(s.charAt(j), '0');
+				
+				
+			}
 			
 		}
-		
-		
-		
-		return " is prime";
-	}
+		map.put(temp, count);
+	}}
+	
+	
+	
+	
+	
+	return map;
+}
+	
+	
+	
+	
 }
