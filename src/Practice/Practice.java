e95964246e3d818f1e068cca427c9e116b8c46b9
Harry2915 <harishhibare2915@gmal.com>
2021-07-04 20:13:02 +0530

    pyramid

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,58 +1,29 @@
 package Practice;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class Practice {
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		System.out.println(countword(s));
-		HashMap<Character, Integer> map= new HashMap<Character, Integer>();
-		map.putAll(countword(s));
-		int max=0;
-		for(Map.Entry<Character, Integer> m : map.entrySet()) {
-			
-			if(m.getValue()>max)
-				max=m.getValue();
-		}
-		for(Map.Entry<Character, Integer> m : map.entrySet()) {
-			if(m.getValue()==max)
-				System.out.println(m.getKey()+" "+m.getValue());
-		}
+for(int i=0;i<5;i++) {
+	for(int k=0;k<4-i;k++)
+		System.out.print(" ");
+	for(int j=0;j<=i;j++) {
+		System.out.print(" * ");
 	}
-public static HashMap<Character, Integer> countword(String s){
-	HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-	for(int i=0;i<s.length();i++) {
-		
-		if(s.charAt(i)!='0' && s.charAt(i)!=' ') {
-			int count=0;
-		char temp=s.charAt(i);
-		for(int j=0;j<s.length();j++) {
-			if(s.charAt(j)==temp && s.charAt(j)!='0') {
-				count++;
-				s.replace(s.charAt(j), '0');
-				
-				
-			}
-			
-		}
-		map.put(temp, count);
-	}}
 	
 	
 	
-	
-	
-	return map;
+	System.out.println();
 }
 	
 	
 	
-	
+	}
 }
