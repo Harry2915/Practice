e89ff49387bc2a2daaf2488a5dd4aa711a3daddc
Harry2915 <harishhibare2915@gmal.com>
2021-07-04 20:13:45 +0530

    rightangled triangle

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -12,8 +12,8 @@ import java.util.Scanner;
 public class Practice {
 	public static void main(String[] args) {
 for(int i=0;i<5;i++) {
-	for(int k=0;k<4-i;k++)
-		System.out.print(" ");
+//	for(int k=0;k<4-i;k++)
+//		System.out.print(" ");
 	for(int j=0;j<=i;j++) {
 		System.out.print(" * ");
 	}
