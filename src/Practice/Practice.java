8528674f9e6ede2cc51b7cf10aa34293663d2dfa
Harry2915 <harishhibare2915@gmal.com>
2021-07-08 20:02:11 +0530

    bubble sort

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,25 +1,44 @@
 package Practice;
 
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Practice {
 	public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			int size = sc.nextInt();
-			int rotation = sc.nextInt();
-			int arr[] = new int[size];
-			for(int i=0;i<size;i++)
-				arr[i]=sc.nextInt();
-			for(int j=size-rotation;j<size;j++)
-				System.out.print(arr[j] + " ");
-			for(int k=0;k<size-rotation;k++)
-				System.out.print(arr[k] + " ");
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[] = new int[n];
+    for(int i =0;i<n;i++) {
+    	arr[i]=sc.nextInt();
+    	
+    }
+ bubble_sort(arr, n);
+ for (int i : arr) {
+	System.out.print(i+" ");
+}
+    }
+	public static int[] bubble_sort(int arr[], int n) {
+		for(int i=0;i<n-1;i++) {
+			for(int j=i+1;j<n;j++) {
+				if(arr[j]<arr[i]) {
+					int temp=arr[j];
+					arr[j]=arr[i];
+					arr[i]=temp;
+				}
+				
+			}
+		}
+		
+		
+		return arr;
 	}
 }
