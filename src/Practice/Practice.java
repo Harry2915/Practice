6a617b2bd8b1e13f05ef07e15ee28ac2e6a2ee85
Harry2915 <harishhibare2915@gmal.com>
2021-07-08 20:15:14 +0530

    selection sort

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -21,24 +21,25 @@ public class Practice {
     	arr[i]=sc.nextInt();
     	
     }
- bubble_sort(arr, n);
+ selection_sort(arr, n);
  for (int i : arr) {
 	System.out.print(i+" ");
 }
     }
-	public static int[] bubble_sort(int arr[], int n) {
+	public static int[] selection_sort(int arr[], int n) {
 		for(int i=0;i<n-1;i++) {
-			for(int j=i+1;j<n;j++) {
-				if(arr[j]<arr[i]) {
-					int temp=arr[j];
-					arr[j]=arr[i];
-					arr[i]=temp;
-				}
-				
-			}
+		int min = i;
+		for(int j=i+1;j<n;j++)
+			if(arr[j]<arr[min])
+				min=j;
+		
+		
+			int temp =arr[min];
+			arr[min]=arr[i];
+			arr[i]=temp;
+			
+		
 		}
-		
-		
 		return arr;
 	}
 }
