ddfc9c6bc969a5165f594c2d08fb7a0171d88a40
Harry2915 <harishhibare2915@gmal.com>
2021-07-15 20:46:58 +0530

    first

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,34 +1,15 @@
 package Practice;
 
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-import java.util.TreeSet;
-class A{
-	int a,b,c;
-	
-	A(int a, int b){
-		this.a=a;
-		this.b=b;
-	System.out.println(a+b);
-	}
-	
-	A(int a, int b,int c){
-		this.a=a;
-		this.b=b;
-		this.c=c;
-		System.out.println(a+b+c);
-	}
-}
+
 public class Practice {
 	public static void main(String[] args) {
-   A a= new A(1,2,3);
+     for(int i=0;i<5;i++) {
+    	 for(int j=0;j<=i;j++)
+    		 System.out.print(" * ");
+    	 
+    	 
+    	 System.out.println();
+     }
 	}
 }
