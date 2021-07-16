81fdb1acd7cadf5bb93ccb05bc5a78f6489ce4c4
Harry2915 <harishhibare2915@gmal.com>
2021-07-16 15:12:36 +0530

    using application context of ioc container

--- firstspring/src/com/controller/EmployeeController.java
+++ firstspring/src/com/controller/EmployeeController.java
@@ -2,14 +2,16 @@ package com.controller;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
 public class EmployeeController {
 public static void main(String[] args) {
-	 Resource resource=new ClassPathResource("applicationContext.xml");  
-	    BeanFactory factory=new XmlBeanFactory(resource);  
-	    
+	 
+	ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
+	    //IoC container
 	    Employee employee = (Employee) factory.getBean("studentbean");
 	   System.out.println( employee.toString());
 }
