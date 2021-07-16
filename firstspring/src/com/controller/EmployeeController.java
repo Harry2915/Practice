76c2faf136abf799604e453b365b5191bdb39708
Harry2915 <harishhibare2915@gmal.com>
2021-07-16 16:54:33 +0530

    autowire

--- firstspring/src/com/controller/EmployeeController.java
+++ firstspring/src/com/controller/EmployeeController.java
@@ -14,7 +14,7 @@ public static void main(String[] args) {
 	 
 	ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
 	    //IoC container
-	    Employee employee = (Employee) factory.getBean("e");
+	    Employee employee = (Employee) factory.getBean("employee");
 	   System.out.println( employee.toString());
 }
 }
