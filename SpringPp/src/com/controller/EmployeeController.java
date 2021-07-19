5c2f7a25c6194f1ee749024c94bb7b760a194f09
Harry2915 <harishhibare2915@gmal.com>
2021-07-19 11:34:07 +0530

    first unseen practice

--- SpringPp/src/com/controller/EmployeeController.java
+++ SpringPp/src/com/controller/EmployeeController.java
@@ -1,7 +1,5 @@
 package com.controller;
 
-
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,10 +7,10 @@ import com.model.Employee;
 
 public class EmployeeController {
 public static void main(String[] args) {
-	 
-	ApplicationContext factory = new ClassPathXmlApplicationContext("applicationContext.xml");
-	    //IoC container
-	    Employee employee = (Employee) factory.getBean("employee");
-	   System.out.println( employee.toString());
+	
+
+	ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+	Employee employee = (Employee) applicationContext.getBean("employee");
+     System.out.println(employee.toString());
 }
 }
