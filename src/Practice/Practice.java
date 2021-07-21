6cb4898db0bf3c29d308eac7e21df96bcffcf0ae
Harry2915 <harishhibare2915@gmal.com>
2021-07-21 09:54:55 +0530

    81 pattern

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -18,17 +18,21 @@ public class Practice {
 
 	public static void main(String[] args)  {
 		
-     for(int j=0;j<=5;j++) {
-    	 int i=65+j;
-    	 
-    	 for(int k=0;k<=j;k++) {
-    		 
-    		 System.out.print((char)i);
-    	    i--;
-    	 }
-    	
-    	 System.out.println();
-     }
+  for(int i=1;i<=5;i++)
+	  
+  {
+	 
+	  for(int j=1;j<=i;j++)
+		  System.out.print(j);
+	  for(int k=0;k<(5-i)*2;k++)
+		  System.out.print(".");
+//	  
+	  for(int m=i;m>=1;m--)
+		  System.out.print(m);
+	  System.out.println();
+	  
+	  
+  }
 	
 }
  
