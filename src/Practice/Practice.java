e86dbae07fe873ee4dffb124712d6b1f85f13498
Harry2915 <harishhibare2915@gmal.com>
2021-07-28 10:22:20 +0530

    ana

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -1,26 +1,30 @@
 package Practice;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Practice {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.next();
-		System.out.println(isPalin(s));
-	}
-	public static String isPalin(String s) {
-		String a ="";
-		for(int i=s.length()-1;i>=0;i--)
-			a+=s.charAt(i);
-		if(a.equalsIgnoreCase(s))
-			return "is palin";
-		else
-		
-		
-		return "not palin";
-	}
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	String s = sc.next();
+	String s1 = sc.next();
+	System.out.println(isana(s,s1));
+}
+public static String isana(String s1, String s2) {
+	char arr1[]=s1.toCharArray();
+	char arr2[]=s2.toCharArray();
+	Arrays.sort(arr1);
+	Arrays.sort(arr2);
+	
+	if(Arrays.equals(arr1,arr2))
+		return "is ana";
+	else
+	
+	
+	return "not ana";
+}
 }
 
