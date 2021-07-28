95fc10dccddd38f83af49256f94e4ffd3f98fd2f
Harry2915 <harishhibare2915@gmal.com>
2021-07-28 10:16:06 +0530

    palin

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -6,104 +6,21 @@ import java.util.Scanner;
 
 public class Practice {
 
-
-
-	public static void main(String[] args)  {
-	List<String> arrayList= new ArrayList<String>();
-	
-	int stm =1;
-	while(stm==1) {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-	System.out.println("Enter File Name:");
-	String s = sc.nextLine();
+		String s = sc.next();
+		System.out.println(isPalin(s));
+	}
+	public static String isPalin(String s) {
+		String a ="";
+		for(int i=s.length()-1;i>=0;i--)
+			a+=s.charAt(i);
+		if(a.equalsIgnoreCase(s))
+			return "is palin";
+		else
+		
+		
+		return "not palin";
+	}
+}
 
-	System.out.println("Select file type");
-	System.out.println("(1)*.txt");
-	System.out.println("(2)*.java");
-	System.out.println("(3)*.png");
-	System.out.println("(4)*.jpeg");
-	
-	int i = sc.nextInt();
-	
-	switch (i) {
-	case 1:     
-		String temp=s+".txt";
-		if(arrayList.contains(temp))
-		{
-			
-			int count=0;
-			for (String string : arrayList) {
-				if(string.equals(temp))
-					count++;
-				temp=s+count+".txt";
-			}
-			String temp2=s+count+".txt";
-			arrayList.add(temp2);
-		}
-		else
-			arrayList.add(temp);
-		break;
-	case 2:
-		String temp1=s+".java";
-		if(arrayList.contains(temp1))
-		{
-			int count2=0;
-			for (String string : arrayList) {
-				if(string.equals(temp1))
-					count2++;
-				temp1=s+count2+".java";
-			}
-			String temp2=s+count2+".java";
-			arrayList.add(temp2);
-		}
-		else
-			arrayList.add(temp1);
-		break;
-		
-	case 3:String tem=s+".png";
-	if(arrayList.contains(tem))
-	{
-		int count=0;
-		for (String string : arrayList) {
-			if(string.equals(tem))
-				count++;
-			tem=s+count+".png";
-		}
-		String temp2=s+count+".png";
-		arrayList.add(temp2);
-	}
-	else
-		arrayList.add(tem);
-	break;
-	case 4:
-		String tem1=s+".jpeg";
-		if(arrayList.contains(tem1))
-		{
-			int count=0;
-			for (String string : arrayList) {
-				if(string.equals(tem1))
-					count++;
-				tem1=s+count+".jpeg";
-			}
-			String temp2=s+count+".jpeg";
-			arrayList.add(temp2);
-		}
-		else
-			arrayList.add(tem1);
-		break;
-		
-	default:
-		break;
-		
-	}
-	System.out.println("Want to add more files?");
-	System.out.println("1.Yes");
-	System.out.println("2.No");
-	int stm1=sc.nextInt();
-	if(stm1!=1)
-		stm=0;
-	}
-	System.out.println(arrayList);
-}
- 
-}
