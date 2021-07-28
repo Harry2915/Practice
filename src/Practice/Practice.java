40dbdbf4ee5fa6c42bcfa04ecadead6acb537d4d
Harry2915 <harishhibare2915@gmal.com>
2021-07-28 10:44:23 +0530

    fibo

--- src/Practice/Practice.java
+++ src/Practice/Practice.java
@@ -9,22 +9,17 @@ public class Practice {
 
 public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
-	String s = sc.next();
-	String s1 = sc.next();
-	System.out.println(isana(s,s1));
-}
-public static String isana(String s1, String s2) {
-	char arr1[]=s1.toCharArray();
-	char arr2[]=s2.toCharArray();
-	Arrays.sort(arr1);
-	Arrays.sort(arr2);
-	
-	if(Arrays.equals(arr1,arr2))
-		return "is ana";
-	else
-	
-	
-	return "not ana";
+	int h = sc.nextInt();
+	int a=0,b=1,c=a+b,i=1;
+	System.out.print("0 ");
+	while(i!=h) {
+		System.out.print(c+" ");
+		b=a;
+		a=c;
+		
+		c=a+b;
+		i++;
+	}
 }
 }
 
